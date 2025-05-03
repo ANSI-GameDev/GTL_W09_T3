ffc869ba644f4e747fab577fb60330800c61f772
wannaBpark <leehun456@hanmail.net>
2025-05-03 17:27:26 +0900

    wip : LODModel 작성중 (vertex, index 버퍼 저장용)

---
+++ EngineTarzan/EngineTarzan/Engine/Source/Runtime/Engine/Rendering/SkeletalMeshLODModel.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "Container/Array.h"
+#include "Math/Color.h"
+#include "Math/Vector4.h"
+
+enum
+{
+    MAX_TEXCOORDS = 4,
+    MAX_TOTAL_INFLUENCES = 4,
+};
+
+struct FSoftSkinVertex
+{
+    FVector Position;
+    FVector TangentX;               // Tangent, U-Direction
+    FVector TangentY;               // BiTangent, V-Direction
+    FVector4 TangentZ;              // Normal
+
+    FVector2D UVs[MAX_TEXCOORDS];   // Texture Coordinates
+    FColor Color;                   // Vertex Color
+
+    uint8 InfluenceBones[MAX_TOTAL_INFLUENCES];     // 영향을 주는 Bone Index Bones
+    float InfluenceWeights[MAX_TOTAL_INFLUENCES];   // 위에 해당하는 가중치 값
+
+    //friend FArchive& operator<<(FArchive& Ar, FSoftSkinVertex& V);
+};
+class FSkeletalMeshLODModel
+{
+public:
+    TArray<FSoftSkinVertex> Vertices;       // 정점 데이터
+    TArray<uint32> Indices;                 // 인덱스 데이터
+
+
+    uint32 NumVertices;
+    uint32 NumTexCoords;
+};
+
