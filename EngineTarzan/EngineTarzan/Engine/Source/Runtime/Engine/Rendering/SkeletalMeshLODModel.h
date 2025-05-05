a850888076239976dd3a83516c7648e331bcf1ed
wannaBpark <leehun456@hanmail.net>
2025-05-05 15:07:34 +0900

    wip : raw data 렌더 데이터로 변환중

--- EngineTarzan/EngineTarzan/Engine/Source/Runtime/Engine/Rendering/SkeletalMeshLODModel.h
+++ EngineTarzan/EngineTarzan/Engine/Source/Runtime/Engine/Rendering/SkeletalMeshLODModel.h
@@ -25,10 +25,23 @@ struct FSoftSkinVertex
     //friend FArchive& operator<<(FArchive& Ar, FSoftSkinVertex& V);
 };
 
+// 섹션 정보 
+struct FSkelMeshSection {
+    uint16       MaterialIndex;     // 머티리얼 슬롯
+    uint32       BaseIndex;         // 인덱스 버퍼 시작 위치
+    uint32       NumTriangles;      // 섹션의 삼각형 개수
+    uint32       BaseVertexIndex;   // 정점 버퍼 시작 위치
+    TArray<int32> BoneMap;          // 이 섹션에서 사용되는 본 인덱스 목록
+};
+
 /** 에디터 내부에서 LOD 편집·쿠킹 로직(FSkeletalMeshBuilder)이 읽어들여 런타임용 FSkeletalMeshRenderData로 변환대상이 될 Raw Data */
 class FSkeletalMeshLODModel
 {
 public:
+    FWString ObjectName;         // LOD 모델 이름
+    FString DisplayName;         // LOD 모델 표시 이름
+
+
     TArray<FSoftSkinVertex>  Vertices;           // 정점 데이터
     TArray<uint32>           Indices;            // 인덱스 데이터
     TArray<uint32>           Faces;              // 폴리곤 인덱스
@@ -40,5 +53,6 @@ public:
     uint32 NumTexCoords;
 
 
+    TArray<FSkelMeshSection> Sections; // 머티리얼별 인덱스 범위와 섹션 단위 본 맵
 };
 
